79d7ffafad853f60b91298b0fc16dd640b41508a
Anders Westrup <anders@westrup.se>
2021-05-24 11:01:42 +0200

    UART R/W

--- src/bin/logicalrust.rs
+++ src/bin/logicalrust.rs
@@ -5,7 +5,8 @@ use logicalrust as _; // global logger + panicking-behavior + memory layout
 use defmt::unwrap;
 
 use stm32f4xx_hal as hal;
-use crate::hal::{prelude::*, stm32};
+use crate::hal::{block, prelude::*, stm32};
+use core::fmt::Write; // for pretty formatting of the serial output
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -17,11 +18,32 @@ fn main() -> ! {
     let mut led = gpioa.pa5.into_push_pull_output();
 
     let rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.sysclk(100.mhz()).freeze();
+    let clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(100.mhz()).freeze();
 
     let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
 
-    for _ in 0..5 {
+    let tx_pin = gpioa.pa2.into_alternate_af7();
+    let rx_pin = gpioa.pa3.into_alternate_af7();
+
+    // configure serial
+    let serial = hal::serial::Serial::usart2(
+        dp.USART2,
+        (tx_pin, rx_pin),
+        hal::serial::config::Config::default().baudrate(115200.bps()),
+        clocks,
+    ).unwrap();
+
+    let (mut tx, mut rx) = serial.split();
+
+    for i in 0..5 {
+        for _ in 0..100 {
+            if let Ok(rec) = rx.read() {
+                defmt::info!("read {}", rec);
+            }
+        }
+        writeln!(tx, "i: {:02}\r", i).unwrap();
+        // block!(tx.flush()).unwrap();
+
         // On for 1s, off for 1s.
         led.set_high().unwrap();
         delay.delay_ms(1000_u32);
