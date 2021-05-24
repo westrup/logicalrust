728e9ac163e967eacd33eb35e2a2482644ad6bfb
Anders Westrup <anders@westrup.se>
2021-05-24 11:01:42 +0200

    Adapt for Nucleo-F411RE

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@
 use core::sync::atomic::{AtomicUsize, Ordering};
 
 use defmt_rtt as _; // global logger
-// TODO(5) adjust HAL import
-// use some_hal as _; // memory layout
+use stm32f4xx_hal as _; // memory layout
 
 use panic_probe as _;
 
