085a621ba1818a98c6962bbad99e20e7b956dd8a
Anders Westrup <anders@westrup.se>
2021-05-24 11:01:42 +0200

    Sump support for ID and META

--- src/bin/logicalrust.rs
+++ src/bin/logicalrust.rs
@@ -6,25 +6,57 @@ use defmt::unwrap;
 
 use stm32f4xx_hal as hal;
 use crate::hal::{prelude::*, stm32};
-use core::fmt::Write; // for pretty formatting of the serial output
+use core::{fmt::Write}; // for pretty formatting of the serial output
+use nb::block;
 
-fn read_with_timeout<S>(timer: &mut hal::timer::Timer<stm32::TIM1>, rx: &mut S) -> Option<u8>
-where S: embedded_hal::serial::Read<u8>,
-{
-    timer.start(1.hz());
-    loop {
-        match rx.read() {
-            Ok(byte) => return Some(byte),
-            Err(hal::nb::Error::WouldBlock) => (),
-            Err(_) => break,
-        }
-        match timer.wait() {
-            Err(hal::nb::Error::WouldBlock) => continue,
-            Ok(_) => break,
-            Err(_) => break,
+enum Sump {
+    Reset,
+    Arm,
+    Id,
+    Test,
+    GetMetadata,
+    RleFinish,
+    XOn,
+    XOff,
+    SetTriggerMask,
+    SetTriggerValues,
+    SetTriggerConf,
+    SetDivider,
+    SetReadDelayCount,
+    SetFlags,
+    Unknown(u8),
+}
+
+enum SumpMeta {
+    End = 0x00,
+    Name = 0x01,
+    SampleMemory = 0x21,
+    DynamicMemory = 0x22,
+    MaxSampleRate = 0x23,
+    NumProbes = 0x40,
+    ProtocolVersion = 0x41,
+}
+
+impl Sump {
+    fn from_byte(byte: u8) -> Sump {
+        match byte {
+            0x00 => Sump::Reset,
+            0x01 => Sump::Arm,
+            0x02 => Sump::Id,
+            0x03 => Sump::Test,
+            0x04 => Sump::GetMetadata,
+            0x05 => Sump::RleFinish,
+            0x11 => Sump::XOn,
+            0x12 => Sump::XOff,
+            0xC0 => Sump::SetTriggerMask,
+            0xC1 => Sump::SetTriggerValues,
+            0xC2 => Sump::SetTriggerConf,
+            0x80 => Sump::SetDivider,
+            0x81 => Sump::SetReadDelayCount,
+            0x82 => Sump::SetFlags,
+            _ => Sump::Unknown(byte),
         }
     }
-    None
 }
 
 #[cortex_m_rt::entry]
@@ -55,21 +87,61 @@ fn main() -> ! {
 
     let (mut tx, mut rx) = serial.split();
 
-    for i in 0..5 {
-        writeln!(tx, "i: {:02}\r", i).unwrap();
-        // block!(tx.flush()).unwrap();
+    unwrap!(led.set_high());
 
-        // On for 1s, off for 1s.
-        led.set_high().unwrap();
-        if let Some(b) = read_with_timeout(&mut timer, &mut rx) {
-            defmt::info!("read {}", b);
-        } else {
-            defmt::info!("timeout");
+    loop {
+        // if let Some(cmd) = read_with_timeout(&mut timer, &mut rx) {
+        match Sump::from_byte(block!(rx.read()).unwrap()) {
+            Sump::Reset => defmt::info!("reset"),
+            Sump::Id => {
+                defmt::info!("ID");
+                write!(tx, "1ALS").unwrap();
+            },
+            Sump::GetMetadata => {
+                defmt::info!("meta");
+
+                block!(tx.write(SumpMeta::Name as u8)).unwrap();
+                write!(tx, "logicalrust").unwrap();
+                block!(tx.write(SumpMeta::End as u8)).unwrap();
+
+                block!(tx.write(SumpMeta::SampleMemory as u8)).unwrap();
+                block!(tx.write(0)).unwrap();
+                block!(tx.write(0)).unwrap();
+                block!(tx.write(1)).unwrap();
+                block!(tx.write(0)).unwrap();
+
+                block!(tx.write(SumpMeta::DynamicMemory as u8)).unwrap();
+                block!(tx.write(0)).unwrap();
+                block!(tx.write(0)).unwrap();
+                block!(tx.write(0)).unwrap();
+                block!(tx.write(0)).unwrap();
+
+                block!(tx.write(SumpMeta::MaxSampleRate as u8)).unwrap();
+                block!(tx.write(0)).unwrap();
+                block!(tx.write(1)).unwrap();
+                block!(tx.write(0)).unwrap();
+                block!(tx.write(0)).unwrap();
+
+                block!(tx.write(SumpMeta::NumProbes as u8)).unwrap();
+                block!(tx.write(0x8)).unwrap();
+
+                block!(tx.write(SumpMeta::ProtocolVersion as u8)).unwrap();
+                block!(tx.write(0x2)).unwrap();
+
+                block!(tx.write(SumpMeta::End as u8)).unwrap();
+            },
+            Sump::Unknown(b) => defmt::info!("unknown {}", b),
+            _ => defmt::info!("unhandled"),
         }
-        // delay.delay_ms(1000_u32);
-        led.set_low().unwrap();
-        delay.delay_ms(1000_u32);
     }
+    // unwrap!(led.set_low());
+
+    // for i in 0..5 {
+    //     writeln!(tx, "i: {:02}\r", i).unwrap();
+    //     // block!(tx.flush()).unwrap();
+
+    //     delay.delay_ms(1000_u32);
+    // }
 
     logicalrust::exit()
 }
